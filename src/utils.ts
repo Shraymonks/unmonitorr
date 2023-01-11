833578f13b74976e0383b05e7bed4fce8721ef1e
Raymond Ha <raymond@shraymonks.com>
2023-01-10 19:40:33 -0800

    Handle undefined Guid

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,7 @@ import type { PlexPayload } from './types/plex';
 // note Metadata.Guid can contain multiple ids of the same type.
 // getGuid([{id: 'tvdb://1234'}], 'tvdb') => ['1234']
 export function getIds(
-  guid: PlexPayload['Metadata']['Guid'],
+  guid: PlexPayload['Metadata']['Guid'] = [],
   type: 'imdb' | 'tmdb' | 'tvdb'
 ): string[] {
   return guid
