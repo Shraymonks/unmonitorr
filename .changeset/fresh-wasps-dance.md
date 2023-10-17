f469732dc0baa41b438defe9041d79c56d15251e
Raymond Ha <raymond@shraymonks.com>
2023-10-17 15:19:25 -0400

    Use undici-types instead of undici

---
+++ .changeset/fresh-wasps-dance.md
@@ -0,0 +1,5 @@
+---
+'unmonitorr': patch
+---
+
+Use undici-types instead of undici
--- src/types/fetch.d.ts
+++ src/types/fetch.d.ts
@@ -1,6 +1,6 @@
 // Workaround for fetch type not yet in @types/node
 // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/60924
 declare global {
-  var fetch: typeof import('undici').fetch;
+  const fetch: typeof import('undici-types').fetch;
 }
 export {};
