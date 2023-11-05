b62613ba459dcc5a6ca1e7e996f3b604d77aea6c
Raymond Ha <raymond@shraymonks.com>
2023-11-04 21:57:16 -0700

    Remove undici-types

--- src/types/fetch.d.ts
+++
@@ -1,6 +0,0 @@
-// Workaround for fetch type not yet in @types/node
-// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/60924
-declare global {
-  const fetch: typeof import('undici-types').fetch;
-}
-export {};
